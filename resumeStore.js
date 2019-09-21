73f96e5553c3ebdc4c6577a84d767c31daab81a2
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-09-21 10:24:18 -0600

    Update token methods

--- resumeStore.js
+++ resumeStore.js
@@ -7,52 +7,31 @@ fs.access(file, fs.constants.F_OK, (err) => {
   console.log(`${file} ${err ? 'does not exist' : 'exists'}`)
 })
 
-// // Check if the file is readable.
-// fs.access(file, fs.constants.R_OK, (err) => {
-//   console.log(`${file} ${err ? notReadable() : isReadable()}`)
-// })
-let readable
-
 // Check if the file is readable.
-fs.access(file, fs.constants.R_OK, (err) => {
-  // console.log(`${file} ${err ? readable = false : readable = true}`)
-  console.log(`${file} ${err ? readable = false : readable = true}`)
-  if (readable) {
-    isReadable()
-  } else if (!readable) {
-    notReadable()
-  }
-})
-
-// set as bool variables
-
-const notReadable = () => {
-  console.log(`Resume file is NOT readable`)
-}
-
-const isReadable = () => {
-  console.log(`Resume file is readable`)
+const readfile = (callback) => {
+  fs.access(file, fs.constants.R_OK, (err) => {
+    const readable = err ? false : true
+    console.log(`rsStatus: ${readable}`)
+    console.log(`errStatus: ${err}`)
+    callback(readable)
+  })
 }
 
 const storeToken = (resumeToken) => {
   fs.writeFile(file, JSON.stringify(resumeToken), 'utf8', (err) => {
     if (err) throw err
-    console.log(`Token stored`)
+    console.log(`rsToken stored`)
   })
 }
 let resumeToken
 
 const getToken = (callback) => {
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, 'utf8', (err, token) => {
     if (err) throw err
-    resumeToken = JSON.parse(data)
-    console.log(`Token: ${resumeToken}`)
+    resumeToken = JSON.parse(token)
+    // console.log(`rsToken: ${JSON.stringify(resumeToken)}`)
     callback(resumeToken)
   })
 }
 
-storeToken('ABC')
-
-getToken((resumeToken) => {
-  console.log(`Returned: ${resumeToken}`)
-})
+module.exports = { getToken, storeToken, readfile }
